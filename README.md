8ba1dd0ae60fff5fefb9fc49cf732c15736ead9f
Lucas Rennó Kallás <lucasrk@unifei.edu.br>
2020-07-18 18:45:04 -0300

    v3

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# chat_py
+# Chat
+
+Versions of a chat in python.
+
+## V1
+Simple, server receive and client send and after client receive and server send.
+
+To use: *python simple_client.py* or *python simple_server.py*, it depends if you want to host or not.
+And if you want to be the first to send a mesage.
+
+## V2
+The same as the first version, but there is the "online" class that can do all the process of receiving and sending a mesage.
+
+To use: *python main.py* and you decide if you want to be the server or the client.
+
+## V3
+I pretend to use threads to receive and send mesages at the same time...
---
+++ v3/main.py
@@ -0,0 +1,49 @@
+from online import online
+import threading
+import time
+
+instance = None
+received = '1'
+sent = '1'
+
+def receiving():
+    global received
+    global sent
+    while(received!='0' and sent!='0'):
+        received = instance.any_receive_mesage()
+        print("Someone: ", received)
+
+def sending():
+    global sent
+    global received
+    while(sent!='0' and received!='0'):
+        sent = input("")
+        #print("Me: ", sent)
+        instance.any_send_mesage(sent)
+
+choose = input("1-Server\n Any-Client\n")
+max_connections = 3
+max_bytes = 1024
+port = 1502
+host = ""
+
+if(choose=='1'):
+    instance = online(host, port, max_connections, max_bytes, True)
+    instance.start_server()
+else:
+    host = input("server ip: ")
+    instance = online(host, port, max_connections, max_bytes, False)
+    instance.start_client()
+
+print('\n-Status: CHAT ON-\n')
+
+t1 = threading.Thread(target=receiving)
+t2 = threading.Thread(target=sending)
+t1.start()
+t2.start()
+
+while(t1.is_alive() and t2.is_alive()):
+    time.sleep(1)
+
+print("\nChat ended.")
+
