88fcad0d2d53fd40a47d2651e7bb17891830affb
Lucas Rennó Kallás <lucasrk@unifei.edu.br>
2020-07-18 21:22:05 -0300

    Added the name

--- v3/main.py
+++ v3/main.py
@@ -5,20 +5,26 @@ import time
 instance = None
 received = '1'
 sent = '1'
+name = input("What's your name?\n> ")
 
 def receiving():
     global received
     global sent
     while(received!='0' and sent!='0'):
         received = instance.any_receive_mesage()
-        print("Someone: ", received)
+        print(received)
 
 def sending():
     global sent
     global received
+    global name
+    instance.any_send_mesage("*"+name+" joined the chat*\n")
     while(sent!='0' and received!='0'):
         sent = input("")
-        instance.any_send_mesage(sent)
+        if(sent=="0"):
+            instance.any_send_mesage("*"+name+" left the chat*\n> Type Enter to Quit\n")
+            break
+        instance.any_send_mesage(name+": "+sent)
 
 choose = input("1-Server\n Any-Client\n")
 max_connections = 2
@@ -30,11 +36,11 @@ if(choose=='1'):
     instance = online(host, port, max_connections, max_bytes, True)
     instance.start_server()
 else:
-    host = input("server ip: ")
+    host = input("Chat IP: ")
     instance = online(host, port, max_connections, max_bytes, False)
     instance.start_client()
 
-print('\n-Status: CHAT ON-\n')
+print('\n-Status: CHAT ON-\n*Type \'0\' and \'Enter\' to quit the chat*\n')
 
 t1 = threading.Thread(target=receiving)
 t2 = threading.Thread(target=sending)
