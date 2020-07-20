55033292c76b36adaf501e394caf31357cbbeeae
Lucas Rennó Kallás <lucasrk@unifei.edu.br>
2020-07-20 14:51:39 -0300

    Server can close

--- v4/main_server.py
+++ v4/main_server.py
@@ -4,30 +4,48 @@ import time
 
 time_1 = 0
 time_clean = 3
+chat_cancel = False
 buffer = "\n\n\n\n\n\n\n*CHAT STARTS HERE*\n\n"
+host_server = input("-> Host IP: ")
 
 def clean_buffer():
     global time_1
     global time_clean
     global buffer
-    while(1):
+    global chat_cancel
+    while(chat_cancel==False):
         time.sleep(1)
         time_1+=1
         if(time_1>=time_clean):
             if(len(buffer)>1024):
-                buffer="\n\n\n\n\n\n\n*CHAT STARTS HERE*\n\n"+buffer[512:]+"\n..."
+                buffer="\n\n\n\n\n\n\n*CHAT STARTS HERE*\n\n..."+buffer[524:]
+
+def can_cancel():
+    global chat_cancel
+    global host_server
+    while(chat_cancel==False):
+        print("\n--If you want to close the server: Type \'0\' and Enter.--")
+        opt = input("")
+        if(opt=='0'):
+            chat_cancel = True
+            print("-- Closing --")
+            close = "__Close__"
+            server_socket.sendto(close.encode(), (host_server,1500))
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 local = ("",1500)
 server_socket.bind(local)
-print("Online Server")
-print("Waiting for data...\n")
 mesage = ""
 
 t1 = threading.Thread(target=clean_buffer)
 t1.start()
 
-while(1):
+t2 = threading.Thread(target=can_cancel)
+t2.start()
+
+print("\n\nOnline Server")
+print("Waiting for data...\n")
+while(chat_cancel==False):
     receive,client = server_socket.recvfrom(2048)
     mesage = receive.decode() 
     if(mesage==""):
@@ -35,5 +53,7 @@ while(1):
     else:
         buffer+="\n"+mesage
         print(buffer)
+
+print("\n\n** Server Closed **")
 
 server_socket.close()
